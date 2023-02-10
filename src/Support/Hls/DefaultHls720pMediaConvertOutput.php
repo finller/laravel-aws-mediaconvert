3d1114c9d119b310bfcc33ff47f6561cabf6273a
Quentin Gabriele <quentin.gabriele@polyconseil.fr>
2023-02-10 18:11:55 +0100

    optimize HLS settings

--- src/Support/Hls/DefaultHls720pMediaConvertOutput.php
+++ src/Support/Hls/DefaultHls720pMediaConvertOutput.php
@@ -5,23 +5,21 @@ namespace Finller\AwsMediaConvert\Support\Hls;
 class DefaultHls720pMediaConvertOutput
 {
     public static function make(
-        string $NameModifier = '-$h$-$f$fps-$rv$kbps'
+        string $NameModifier = '-$h$p-$f$fps-$rv$kbps'
     ): array {
         return [
-            'ContainerSettings' => [
-                'Container' => 'M3U8',
-                'M3u8Settings' => [
-                    'AudioFramesPerPes' => 4,
-                    'PcrControl' => 'PCR_EVERY_PES_PACKET',
-                    'PmtPid' => 480,
-                    'PrivateMetadataPid' => 503,
-                    'ProgramNumber' => 1,
-                    'PatInterval' => 0,
-                    'PmtInterval' => 0,
-                    'Scte35Pid' => 500,
-                    'TimedMetadataPid' => 502,
-                    'VideoPid' => 481,
-                    'AudioPids' => [
+            "ContainerSettings" => [
+                "Container" => "M3U8",
+                "M3u8Settings" => [
+                    "AudioFramesPerPes" => 4,
+                    "PcrControl" => "PCR_EVERY_PES_PACKET",
+                    "PmtPid" => 480,
+                    "PrivateMetadataPid" => 503,
+                    "ProgramNumber" => 1,
+                    "PatInterval" => 0,
+                    "PmtInterval" => 0,
+                    "VideoPid" => 481,
+                    "AudioPids" => [
                         482,
                         483,
                         484,
@@ -38,82 +36,75 @@ class DefaultHls720pMediaConvertOutput
                         495,
                         496,
                         497,
-                        498,
-                    ],
-                ],
+                        498
+                    ]
+                ]
             ],
-            'VideoDescription' => [
-                'ScalingBehavior' => 'DEFAULT',
-                'Height' => 720,
-                'TimecodeInsertion' => 'DISABLED',
-                'AntiAlias' => 'ENABLED',
-                'Sharpness' => 50,
-                'CodecSettings' => [
-                    'Codec' => 'H_264',
-                    'H264Settings' => [
-                        'InterlaceMode' => 'PROGRESSIVE',
-                        'ParNumerator' => 1,
-                        'NumberReferenceFrames' => 3,
-                        'Syntax' => 'DEFAULT',
-                        'FramerateDenominator' => 1001,
-                        'GopClosedCadence' => 1,
-                        'HrdBufferInitialFillPercentage' => 90,
-                        'GopSize' => 90,
-                        'Slices' => 1,
-                        'GopBReference' => 'DISABLED',
-                        'HrdBufferSize' => 9750000,
-                        'SlowPal' => 'DISABLED',
-                        'ParDenominator' => 1,
-                        'SpatialAdaptiveQuantization' => 'ENABLED',
-                        'TemporalAdaptiveQuantization' => 'ENABLED',
-                        'FlickerAdaptiveQuantization' => 'DISABLED',
-                        'EntropyEncoding' => 'CABAC',
-                        'Bitrate' => 6500000,
-                        'FramerateControl' => 'SPECIFIED',
-                        'RateControlMode' => 'CBR',
-                        'CodecProfile' => 'MAIN',
-                        'Telecine' => 'NONE',
-                        'FramerateNumerator' => 30000,
-                        'MinIInterval' => 0,
-                        'AdaptiveQuantization' => 'HIGH',
-                        'CodecLevel' => 'LEVEL_3_1',
-                        'FieldEncoding' => 'PAFF',
-                        'SceneChangeDetect' => 'ENABLED',
-                        'QualityTuningLevel' => 'MULTI_PASS_HQ',
-                        'FramerateConversionAlgorithm' => 'DUPLICATE_DROP',
-                        'UnregisteredSeiTimecode' => 'DISABLED',
-                        'GopSizeUnits' => 'FRAMES',
-                        'ParControl' => 'SPECIFIED',
-                        'NumberBFramesBetweenReferenceFrames' => 1,
-                        'RepeatPps' => 'DISABLED',
-                    ],
-                ],
-                'AfdSignaling' => 'NONE',
-                'DropFrameTimecode' => 'ENABLED',
-                'RespondToAfd' => 'NONE',
-                'ColorMetadata' => 'INSERT',
-            ],
-            'AudioDescriptions' => [
-                [
-                    'AudioTypeControl' => 'FOLLOW_INPUT',
-                    'CodecSettings' => [
-                        'Codec' => 'AAC',
-                        'AacSettings' => [
-                            'AudioDescriptionBroadcasterMix' => 'NORMAL',
-                            'Bitrate' => 96000,
-                            'RateControlMode' => 'CBR',
-                            'CodecProfile' => 'HEV1',
-                            'CodingMode' => 'CODING_MODE_2_0',
-                            'RawFormat' => 'NONE',
-                            'SampleRate' => 48000,
-                            'Specification' => 'MPEG4',
+            "VideoDescription" => [
+                "ScalingBehavior" => "DEFAULT",
+                "Height" => 720,
+                "TimecodeInsertion" => "DISABLED",
+                "AntiAlias" => "ENABLED",
+                "Sharpness" => 50,
+                "CodecSettings" => [
+                    "Codec" => "H_264",
+                    "H264Settings" => [
+                        "InterlaceMode" => "PROGRESSIVE",
+                        "NumberReferenceFrames" => 3,
+                        "Syntax" => "DEFAULT",
+                        "GopClosedCadence" => 1,
+                        "GopSize" => 90,
+                        "Slices" => 1,
+                        "GopBReference" => "DISABLED",
+                        "MaxBitrate" => 8000000,
+                        "SpatialAdaptiveQuantization" => "ENABLED",
+                        "TemporalAdaptiveQuantization" => "ENABLED",
+                        "FlickerAdaptiveQuantization" => "DISABLED",
+                        "EntropyEncoding" => "CABAC",
+                        "RateControlMode" => "QVBR",
+                        "QvbrSettings" => [
+                            "QvbrQualityLevel" => 7,
+                            "QvbrQualityLevelFineTune" => 0
                         ],
-                    ],
-                    'LanguageCodeControl' => 'FOLLOW_INPUT',
-                    'AudioType' => 0,
+                        "CodecProfile" => "MAIN",
+                        "MinIInterval" => 0,
+                        "AdaptiveQuantization" => "HIGH",
+                        "CodecLevel" => "AUTO",
+                        "FieldEncoding" => "PAFF",
+                        "SceneChangeDetect" => "ENABLED",
+                        "QualityTuningLevel" => "SINGLE_PASS",
+                        "UnregisteredSeiTimecode" => "DISABLED",
+                        "GopSizeUnits" => "FRAMES",
+                        "NumberBFramesBetweenReferenceFrames" => 2,
+                        "RepeatPps" => "DISABLED"
+                    ]
                 ],
+                "AfdSignaling" => "NONE",
+                "DropFrameTimecode" => "ENABLED",
+                "RespondToAfd" => "NONE",
+                "ColorMetadata" => "INSERT"
             ],
-            'NameModifier' => $NameModifier,
+            "AudioDescriptions" => [
+                [
+                    "AudioTypeControl" => "FOLLOW_INPUT",
+                    "AudioSourceName" => "Audio Selector 1",
+                    "CodecSettings" => [
+                        "Codec" => "AAC",
+                        "AacSettings" => [
+                            "AudioDescriptionBroadcasterMix" => "NORMAL",
+                            "Bitrate" => 96000,
+                            "RateControlMode" => "CBR",
+                            "CodecProfile" => "LC",
+                            "CodingMode" => "CODING_MODE_2_0",
+                            "RawFormat" => "NONE",
+                            "SampleRate" => 48000,
+                            "Specification" => "MPEG4"
+                        ]
+                    ],
+                    "LanguageCodeControl" => "FOLLOW_INPUT"
+                ]
+            ],
+            "NameModifier" => $NameModifier
         ];
     }
 }
