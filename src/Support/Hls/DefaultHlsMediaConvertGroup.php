22ec5c9a362e53d96eed732d27c525fa4cb7364d
QuentinGab <QuentinGab@users.noreply.github.com>
2023-02-10 14:32:37 +0000

    Fix styling

--- src/Support/Hls/DefaultHlsMediaConvertGroup.php
+++ src/Support/Hls/DefaultHlsMediaConvertGroup.php
@@ -40,7 +40,7 @@ class DefaultHlsMediaConvertGroup implements Arrayable
 
     public function addOutputWhen(mixed $condition, callable|array $Output): static
     {
-        if (!$condition) {
+        if (! $condition) {
             return $this;
         }
 
